44d5f99ae8bbf214aeded3e0ce9e61dfcef9546b
fall1600 <fall1600@gmail.com>
2018-07-26 00:00:03 +0800

    foo banner

---
+++ src/components/FooBanner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="title">不知道要放什麼, 但我就是想放</h1>
+  </div>
+</template>
+
+<style scoped>
+  .title {
+    background-color: red;
+    color: aliceblue;
+  }
+</style>
