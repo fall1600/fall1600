673bd6701280ca48f94487a1ccf4b63dceffeb83
fall1600 <fall1600@gmail.com>
2018-07-28 12:14:48 +0800

    引入bulma

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="../node_modules/bulma/bulma.sass"></style>
+<style lang="sass" scoped>
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
 </style>
