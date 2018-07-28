9df189a33d675e646623c2950b0c96794cf0b72d
fall1600 <fall1600@gmail.com>
2018-07-28 12:15:12 +0800

    開始來切吧

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,15 +2,17 @@
   <div class="container">
     <div class="notification">
       <Namecard/>
+      <!-- <UbeeIO/> -->
     </div>
   </div>
 </template>
 
 <script>
 import Namecard from './Namecard.vue'
+import UbeeIO from './UbeeIO.vue'
 export default {
   components: {
-    Namecard
+    Namecard, UbeeIO
   }
 }
 </script>
--- src/components/Namecard.vue
+++ src/components/Namecard.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="columns">
-    <div class="is-one-third">
-      <div class="card">
-        <div class="card-image">
-          <figure class="image is-4by3">
-            <img src="../assets/fall1600.jpg" alt="Placeholder image">
-          </figure>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image">
+          <img src="../assets/fall1600.jpg" alt="Avatar of fall1600">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">林璟鴻 / fall1600</p>
+            <p class="subtitle is-6">
+              Backend engineer <a target="blank" href="https://symfony.com/">#Symfony</a> <a target="blank" href="https://vuejs.org/">#Vue</a>
+            </p>
+          </div>
         </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">林璟鴻 / fall1600</p>
-              <p class="subtitle is-6">
-                Backend engineer <a target="blank" href="https://symfony.com/">#Symfony</a> <a target="blank" href="https://vuejs.org/">#Vue</a>
-              </p>
-            </div>
-          </div>
-          <div class="content">
-            <p>喜歡寫網頁上看不到的東西, 提供前端舒服的api</p> 
-          </div>
+        <div class="content">
+          <p>喜歡寫網頁上看不到的東西, 提供前端舒服的api</p> 
         </div>
       </div>
     </div>
@@ -26,8 +24,9 @@
 </template>
 
 <style lang='sass' scoped>
-figure.image > img 
-  border-radius: 1%
+figure.image > img
+  max-height: 600px
+  width: auto
   filter: blur(2px)
 
 figure.image > img:hover 
