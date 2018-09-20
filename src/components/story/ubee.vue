3f16e0590f5db707ba433a48406012c66a5d10c4
jhlin <fall1600@gmail.com>
2018-09-20 23:54:53 +0800

    start story of ubee

--- src/components/story/ubee.vue
+++ src/components/story/ubee.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>ubee</div>
+  <div class="container">
+    <section class="hero">
+      <div class="hero-body">
+        <h1 class="title">Ubee</h1>
+        <h2 class="subtitle">地圖找房, 金流</h2>
+      </div>
+    </section>
+    <div>
+      地圖找房, 使用 <a href="https://developers.google.com/maps/documentation/?hl=zh-tw" target="_blank">google map</a>, <a href="https://googlemaps.github.io/js-rich-marker/examples/richmarker.html" target="_blank">Rick Marker</a>,
+      <a href="https://github.com/googlemaps/js-marker-clusterer" target="_blank">js-marker-clusterer</a>
+      <p>
+        <img src="../../assets/ubee_map.png" alt="">
+      </p>
+      <hr>
+    </div>
+  </div>
 </template>
