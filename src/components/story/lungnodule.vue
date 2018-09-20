a05977f3f47afc6fa04b53b4913c7e49d1c275ed
jhlin <fall1600@gmail.com>
2018-09-20 22:56:35 +0800

    arrange detail

--- src/components/story/lungnodule.vue
+++ src/components/story/lungnodule.vue
@@ -19,19 +19,20 @@
       <hr>
       背景程式, 使用: <a href="http://gearman.org/" target="_blank">gearman</a>
       <p>
-        續傳成功後, 產生job, 交由gearman job server 分派, 並註冊對應worker 接手處理 <br>
-        另外, 分析資料處理規則後, 我將可拆的工作項目拆小, 額外規劃工作項目, 便能更完善地用到server 效能
-      </p>
-      tune 畫面
-      <p>
-        因應業務邏輯需在畫面上快速切換前後幾張圖片做比對, 因此快速載入圖片便為此調校項目 <br>
-        除了前端手動預先發api 得到前後物件, 也可採用http2 preload 機制, server 在response header 下preload 的Link, 便能讓前端預先得到下一張圖
+        續傳成功後, gearman client產生 job, 交由gearman job server 分派, 並註冊對應worker 接手處理 <br>
+        另外, 分析資料處理規則後, 我將工作項目拆解, 規劃job, 便能更完善地用到server 效能(根據worker 數量)
       </p>
       <hr>
       Python to app, 使用: <a href="https://pypi.org/project/py2exe/" target="_blank">py2exe</a>, <a href="https://py2app.readthedocs.io/en/latest/" target="_blank">py2app</a>
       <p>
         因部份資料處理的邏輯必須跑在客戶端, 所以提供Windows, Mac上的應用程式 <br>
         以python, 在研究py2exe, py2app期間踩了一些環境上的地雷 <a href="https://drive.google.com/drive/folders/18p9OZtv3-zDq3oMnbGcsd-2xSiODnsZa?usp=sharing" target="_blank">[筆記]</a>
+      </p>
+      <hr>
+      Tune 畫面
+      <p>
+        因應業務邏輯需在畫面上快速切換前後幾張圖片做比對, 因此快速載入圖片便為此調校項目 <br>
+        除了前端手動預先發api 得到前後物件, 也可採用http2 preload 機制, server 在response header 下preload 的Link, 便能讓前端預先得到下一張圖
       </p>
     </div>
   </div>
