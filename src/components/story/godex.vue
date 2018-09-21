a84f74933cc5fc77219f1d40af7c9b1fd33cb1de
jhlin <fall1600@gmail.com>
2018-09-22 00:15:04 +0800

    story of godex

--- src/components/story/godex.vue
+++ src/components/story/godex.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>godex</div>
+  <div class="container">
+    <section class="hero">
+      <div class="hero-body">
+        <h1 class="title">Godex</h1>
+        <h2 class="subtitle">產品及其關聯資料, 多語系</h2>
+      </div>
+    </section>
+    <div>
+      資料庫schema 開發, 使用 <a href="http://propelorm.org/" target="_blank">Propel</a> <br>
+      <p>
+        由產品為原點設計關聯資料的規劃與實現, 並支援資料上的多語系(*_i18n) <br>
+        <img src="../../assets/godex_product.png" alt=""> <hr>
+        利用propel 提供的hook function(preSave, preInsert...) 做到應用層級的邏輯設定 <br>
+        舉例: 新增產品規格, 可在存入資料庫前抓出相同分類的最接近規格做預設排序, 但非寫在自身儲存邏輯 <br>
+        <img src="../../assets/godex_hook.png" alt=""> <br><br>
+        自身儲存邏輯, 因應上圖(schema) 的資料關聯, 藉由發送event 達到程式邏輯解耦合 <br>
+        <img src="../../assets/godex_create_spec.png" alt="">
+      </p>
+      <hr>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  created: () => {
-    alert("gg")
-  }
-}
-</script>
