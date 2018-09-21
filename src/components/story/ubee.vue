d6004178ec752c452a96fe8a6213c938018079b9
jhlin <jhlin@dgfactor.com.tw>
2018-09-21 15:03:27 +0800

    發送商品邏輯

--- src/components/story/ubee.vue
+++ src/components/story/ubee.vue
@@ -17,7 +17,7 @@
       <p>
         利用Symfony 提供的di 達到物件設計的單一職責, 有需要別的物件功能再注入, 避免寫出天神物件, 好寫也好測 <br>
         跨模組之間流程則用事件的方式傳遞, 讓註冊事件的物件(listener) 接收, 再派發或自行處理, 以下兩例說明 <br><br>
-        舉例1: 收到錢之後的業務邏輯<br>
+        舉例1: 收到錢之後的邏輯<br>
         <img src="../../assets/ubee_event_recieve_order.png" alt="">
         <ul>
           <li>系統收到錢之後, 付款模組payment(1024)先接收到事件, 紀錄金流回call 的log, 付款狀態...等</li>
@@ -27,8 +27,15 @@
         </ul>
         <br>
 
-        舉例2 :
+        舉例2: 發送商品邏輯 <br>
         <img src="../../assets/ubee_event_delivery_order.png" alt="">
+        <ul>
+          <li>房屋仲介資料異動</li>
+          <li>房屋仲介業務額度提高</li>
+          <li>商品方案名稱變更</li>
+          <li>商品點數增加</li>
+        </ul>
+        <br>
       </p>
 
     </div>
