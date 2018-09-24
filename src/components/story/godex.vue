d5a25d92dcc0f3ba02a5cc68216c36af808d0647
jhlin <fall1600@gmail.com>
2018-09-25 07:30:24 +0800

    補充debug:event-dispatcher on godex

--- src/components/story/godex.vue
+++ src/components/story/godex.vue
@@ -10,12 +10,18 @@
       資料庫schema 開發, 使用 <a href="http://propelorm.org/" target="_blank">Propel</a> <br>
       <p>
         由產品為原點設計關聯資料的規劃與實現, 並支援資料上的多語系(*_i18n) <br>
-        <img src="../../assets/godex_product.png" alt=""> <hr>
+        <img src="../../assets/godex_product.png" alt="">
+      </p>
+      <hr>
+      <p>
         利用propel 提供的hook function(preSave, preInsert...) 做到應用層級的邏輯設定 <br>
         舉例: 新增產品規格, 可在存入資料庫前抓出相同分類的最接近規格做預設排序, 但非寫在自身儲存邏輯 <br>
         <img src="../../assets/godex_hook.png" alt=""> <br><br>
-        自身儲存邏輯, 因應上圖(schema) 的資料關聯, 藉由發送event 達到程式邏輯解耦合 <br>
-        <img src="../../assets/godex_create_spec.png" alt="">
+        產品規格自身儲存邏輯, 因應上圖(schema) 的資料關聯, 藉由發送event 達到程式邏輯解耦合 <br>
+        <img src="../../assets/godex_create_spec.png" alt=""> <br><br>
+        清查註冊接聽此事件的listener <br>
+        $ app/console debug:event-dispatcher
+        <img src="../../assets/godex_create_spec_event.png" alt="">
       </p>
       <hr>
     </div>
