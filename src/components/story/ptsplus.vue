e51af5d455d013acbe92afc233275e586756fee2
jhlin <fall1600@gmail.com>
2018-09-25 08:02:28 +0800

    補充一點page view 加一的概念

--- src/components/story/ptsplus.vue
+++ src/components/story/ptsplus.vue
@@ -13,6 +13,16 @@
         memcache 中以linked-list 概念紀錄單位時間中的page view, 處理各種情況的page view 加一
         <a href="https://docs.google.com/presentation/d/1QXNfivxRXH5pCn3jzbpkOuiE0RFSXbs7boHmC9gXnBY/edit" target="_blank">[筆記]</a>
       </p>
+      思維如下,
+      <ul>
+        <li>case1: 已經存在list 中的加一</li>
+        <li>case2: 尚未存在list 中的加一</li>
+        <li>case3: list 有資料時的新增一個節點</li>
+        <li>case4: list 無資料時的新增一個節點(含初始list)</li>
+      </ul>
+      <br>
+      排程回寫資料庫的處理: <br>
+      回寫期間建立另一條list 做紀錄, 達到page view 加一與回寫機制同時運作
       <hr>
     </div>
   </div>
