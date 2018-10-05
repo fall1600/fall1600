d166eead8bc54eeb173c70dd89aa842953d9faf2
jhlin <fall1600@gmail.com>
2018-10-06 00:49:21 +0800

    照片處理顏色; ukik 簡易說明

--- src/components/story/uknowiknow.vue
+++ src/components/story/uknowiknow.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <h1 v-if="mode==='interview'" class="title">Uknowiknow</h1>
-    <h1 v-else-if="mode==='guest'" class="title">課程平台</h1>
+  <div class="container">
+    <section class="hero">
+      <div class="hero-body">
+        <h1 v-if="mode==='interview'" class="title">Uknowiknow</h1>
+        <h1 v-else-if="mode==='guest'" class="title">課程平台</h1>
+      </div>
+    </section>
+
+    <div>
+      簡介: 課程報名系統 <br>
+      說明: 因此專案為我進數位因子的第一個專案, 當時開發進度進尾聲, 故我負責的範圍只有前端套資料 <br>
+      <img src="../../assets/school_main.png" alt="">
+      <br>
+    </div>
   </div>
 </template>
 
