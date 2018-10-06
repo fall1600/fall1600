553d35842cabc1e3129413c02592a040f92827bd
jhlin <fall1600@gmail.com>
2018-10-07 01:37:19 +0800

    回家吧孩子 : )

--- src/components/page404.vue
+++ src/components/page404.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="container has-text-centered">
     <iframe src="https://404page.missingkids.org.tw/api?key=675gF8PTcnh7X7YjRtaT" width="100%" height="635" frameborder="0"></iframe>
+    <router-link :to="{name: 'root'}" class='button is-danger'>回家吧孩子 : )</router-link>
   </div>
 </template>
