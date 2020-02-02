bea4e8d2d8f57c991bc0a2ddd346e51112e68505
fall1600 <fall1600@gmail.com>
2020-02-02 21:49:05 +0800

    說明handsup

--- src/components/route/story/handsup.vue
+++ src/components/route/story/handsup.vue
@@ -9,17 +9,22 @@
           </a>
         </h1>
         <h1 v-else-if="mode==='guest'" class="title">直播電商</h1>
-        <h2 class="subtitle">7-11 物流系統串接</h2>
+        <h2 class="subtitle">7-11 物流服務串接</h2>
       </div>
     </section>
 
     <div>
-      簡介: 實作串接完整7-11 物流<br>
-      <img src="../../../assets/story/product_main.png" alt="">
+      簡介: 實作串接完整7-11 物流 <br><br>
+      <img src="../../../assets/story/handsdown_main.png" alt="">
       <br>
     </div>
     <hr>
     <div>
+      電商領域中, 物流可說是訂單狀態走到盡頭的最後一哩路, 讓買家在付錢後能順利收貨 <br>
+      物流服務就是一種真實世界的非同步請求, 跟物流商請求要號之後, 便開始等待物流商通知狀態變化 <br>
+      7-11 資料普遍都是靠定時回檔告知物流單狀態, 實作上依照規格書定義的回檔時間執行撈取特定檔案比對與訂單異動即可 <br><br>
+      <img src="../../../assets/story/handsdown_7-11.png" alt="">
+      <hr>
     </div>
   </div>
 </template>
